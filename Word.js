3dc2562b1f26116920923bd5e5a33c9119cded80
William Gonzales <wgonz94@yahoo.com>
2019-10-08 13:52:11 -0700

    constructed function for word guessing input by letter

---
+++ Word.js
@@ -0,0 +1,26 @@
+var Letter = require("./letter.js");
+
+function Word(answer){
+    this.letterObj = [];
+
+    for (var i = 0; i < answer.length; i++) {
+        var letterOpt = new Letter(answer[i]);
+        this.letterObj.push(letterOpt);
+    }
+
+    this.log = function () {
+        answerConsLog = " ";
+        for (var i = 0; i < this.letterObj.length; i++) {
+            answerConsLog += this.letterObj[i] + " ";
+        }
+        
+    }
+
+    this.input = function (input) {
+        for (var i = 0; i < this.letterObj.length; i++) {
+            this.letterObj.guess(input)
+        }
+    }
+}
+
+module.exports = Word;
